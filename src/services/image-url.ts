f052f5a3484e6966ecc1055681ed07fc9a61575e
Jenilyn Sanchez <jenilynsanchez@gmail.com>
2023-08-15 14:05:24 +0800

    handle game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,5 @@
 const getCroppedImageUrl = (url: string, w: number, h: number) => {
-  if (!url) return "";
+  if (!url) return "https://placehold.co/600x400/EEE/31343C";
   
   const originalUrl = url.split("/media/");
   const croppedUrl = `${originalUrl[0]}/media/crop/${w}/${h}/${originalUrl[1]}`;
